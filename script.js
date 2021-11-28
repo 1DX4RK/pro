cd3ece9ad6bf35a04de04d8ba904c3bd618e9271
DX4RK <91917093+DX4RK@users.noreply.github.com>
2021-11-28 22:20:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.typed', {
+  strings: ["!help", "@Link Protection", "Multifunctional Discord Bot", "80+ Blacklisted loggers links", "Really practical", "Effective", "Easy To Use"],
+  typeSpeed: 80,
+  backSpeed: 70,
+  loop: true,
+  shuffle: true,
+  backDelay: 2,
+  contentType: 'html'
+});
